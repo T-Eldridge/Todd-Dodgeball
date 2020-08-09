5414d8ea931de8e77644e159b33e3dbf2fbd8aad
TEldridge <skippydrumskins@yahoo.com>
2020-08-09 15:18:22 -0500

    michael helped complete dodgeball

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* eslint-disable max-classes-per-file */
 const assert = require("assert")
+const { doc } = require("prettier")
 // Once you understand the challenge, whiteboard the logic.
 // Create a list of steps your app needs to do (code plan).
 // Translate to pseudo code.
@@ -22,10 +23,15 @@ const assert = require("assert")
 const arrOfPeople = [
   {
     id: 2,
-    name: "Charles Young",
-    age: 55,
-    skillSet: "welding",
-    placeBorn: "Omaha, Nebraska",
+    name: "Kate Veatch",
+    age: 31,
+    skillSet: "Can Throw A Ball",
+    placeBorn: "Louisville, Kentucky",
+    canThrowBall: true,
+    canDodgeBall: true,
+    hasPaid: true,
+    isHealthy: true,
+    yearsExperience: 3,
   },
   {
     id: 3,
@@ -33,6 +39,11 @@ const arrOfPeople = [
     age: 35,
     skillSet: "fishing",
     placeBorn: "Louisville, Kentucky",
+    canThrowBall: true,
+    canDodgeBall: true,
+    hasPaid: true,
+    isHealthy: true,
+    yearsExperience: 3,
   },
   {
     id: 4,
@@ -40,13 +51,23 @@ const arrOfPeople = [
     age: 20,
     skillSet: "tic tac toe",
     placeBorn: "Pawnee, Texas",
+    canThrowBall: true,
+    canDodgeBall: true,
+    hasPaid: true,
+    isHealthy: true,
+    yearsExperience: 3,
   },
   {
     id: 5,
     name: "John Willouby",
     age: 28,
-    skillSet: "pipe fitting",
+    skillSet: "CAN DODGE A WRENCH",
     placeBorn: "New York, New York",
+    canThrowBall: true,
+    canDodgeBall: true,
+    hasPaid: true,
+    isHealthy: true,
+    yearsExperience: 3,
   },
   {
     id: 6,
@@ -54,6 +75,11 @@ const arrOfPeople = [
     age: 20,
     skillSet: "boomerang throwing",
     placeBorn: "Perth, Australia",
+    canThrowBall: true,
+    canDodgeBall: true,
+    hasPaid: true,
+    isHealthy: true,
+    yearsExperience: 3,
   },
   {
     id: 7,
@@ -61,6 +87,11 @@ const arrOfPeople = [
     age: 17,
     skillSet: "acrobatics",
     placeBorn: "Los Angeles, California",
+    canThrowBall: true,
+    canDodgeBall: true,
+    hasPaid: true,
+    isHealthy: true,
+    yearsExperience: 3,
   },
   {
     id: 8,
@@ -68,6 +99,11 @@ const arrOfPeople = [
     age: 32,
     skillSet: "jump rope",
     placeBorn: "New Orleans, Louisiana",
+    canThrowBall: true,
+    canDodgeBall: true,
+    hasPaid: true,
+    isHealthy: true,
+    yearsExperience: 3,
   },
 ]
 
@@ -95,7 +131,7 @@ class DodgeBallPlayer {
     this.yearsExperience = yearsExperience
   }
 }
-class GloboGymTeammate {
+class GloboGymTeammate extends DodgeBallPlayer {
   constructor(
     player,
     canThrowBall,
@@ -104,6 +140,14 @@ class GloboGymTeammate {
     isHealthy,
     yearsExperience
   ) {
+    super(
+      player,
+      canThrowBall,
+      canDodgeBall,
+      hasPaid,
+      isHealthy,
+      yearsExperience
+    )
     this.player = player
     this.canThrowBall = canThrowBall
     this.canDodgeBall = canDodgeBall
@@ -112,10 +156,24 @@ class GloboGymTeammate {
     this.yearsExperience = yearsExperience
   }
 }
-class AverageJoesTeammate {
-  constructor(player) {
+class AverageJoesTeammate extends DodgeBallPlayer {
+  constructor(
+    player,
+    canThrowBall,
+    canDodgeBall,
+    hasPaid,
+    isHealthy,
+    yearsExperience
+  ) {
+    super(
+      player,
+      canThrowBall,
+      canDodgeBall,
+      hasPaid,
+      isHealthy,
+      yearsExperience
+    )
     this.player = player
-
     this.canThrowBall = canThrowBall
     this.canDodgeBall = canDodgeBall
     this.hasPaid = hasPaid
@@ -125,32 +183,215 @@ class AverageJoesTeammate {
 }
 // function that populates HTML players
 const listPeopleChoices = () => {
+  const bodyElement = document.getElementById("body")
+  const listButton = document.getElementById("list-people-button")
+  bodyElement.removeChild(listButton)
   const listElement = document.getElementById("people")
+  listElement.className = "people-list"
+  const ineligiblePlayer = document.createElement("LI")
   arrOfPeople.map((person) => {
     const li = document.createElement("li")
     const button = document.createElement("button")
     button.innerHTML = "Make Player"
-    button.addEventListener("click", function () {
-      makePlayer(person.id)
-    })
     li.appendChild(button)
     li.appendChild(
-      document.createTextNode(person.name + " - " + person.skillSet)
+      document.createTextNode(
+        `${person.name}  -  Special Skill: ${person.skillSet}  -  Years of Experience: ${person.yearsExperience}`
+      )
     )
-    listElement.append(li)
-  })
-}
+    if (
+      person.canThrowBall == true &&
+      person.canDodgeBall == true &&
+      person.hasPaid == true &&
+      person.isHealthy == true &&
+      person.yearsExperience > 0
+    ) {
+      listElement.append(li)
+    } else {
+      listElement.append(ineligiblePlayer)
+      ineligiblePlayer.appendChild(button)
+      ineligiblePlayer.appendChild(
+        document.createTextNode(
+          `${person.name}  -  Special Skill: ${person.skillSet}  -  Years of Experience: ${person.yearsExperience}`
+        )
+      )
+      ineligiblePlayer.style.color = "red"
+    }
 
-listPeopleChoices(arrOfPeople)
-// to select a player from the players array to add to a team array
-const makePlayer = (id) => {
-  console.log(`li ${id} was clicked!`)
-}
-if (typeof describe === "function") {
-  describe("DodgeBallPlayer", () => {
-    it("Should add person to listOfPlayers", () => {
-      const player1 = new DodgeBallPlayer("Rick Astley")
-      assert.equal(player1.player, "Rick Astley")
+    button.addEventListener("click", function () {
+      if (
+        person.canThrowBall == true &&
+        person.canDodgeBall == true &&
+        person.hasPaid == true &&
+        person.isHealthy == true &&
+        person.yearsExperience > 0
+      ) {
+        makePlayer(person)
+        li.removeChild(this)
+      } else {
+        ineligiblePlayer.style.textDecoration = "line-through"
+        alert(`${person.name} did not meet all pre-game criteria`)
+        ineligiblePlayer.removeChild(this)
+      }
     })
   })
 }
+//END OF LIST PEOPLE BUTTON //
+listPeopleChoices(arrOfPeople)
+// to select a player from the players array to add to a team array
+// MAKE PLAYER BUTTON //
+const makePlayer = (id) => {
+  const eligibleList = document.getElementById("players")
+  eligibleList.className = "eligible-list"
+  const eligibleLI = document.createElement("LI")
+  const globoList = document.getElementById("globo")
+  const joesList = document.getElementById("joes")
+  const globoLI = document.createElement("LI")
+  const joesLI = document.createElement("LI")
+  globoLI.className = "globo-team-list"
+  joesLI.className = "joes-team-list"
+  const globoButton = document.createElement("BUTTON")
+  const avgJoeButton = document.createElement("BUTTON")
+  globoButton.className = "globo-button"
+  avgJoeButton.className = "joe-button"
+  eligibleLI.append(avgJoeButton)
+  eligibleLI.append(globoButton)
+  eligibleLI.appendChild(document.createTextNode(`${id.name}`))
+  globoButton.innerHTML = "Team Globo Gym"
+  avgJoeButton.innerHTML = "Team Average Joes"
+  eligibleList.append(eligibleLI)
+  globoButton.addEventListener("click", function () {
+    if (
+      globoGym.length < 3 &&
+      id.canThrowBall == true &&
+      id.canDodgeBall == true &&
+      id.hasPaid == true &&
+      id.isHealthy == true &&
+      id.yearsExperience > 0
+    ) {
+      globoGym.push(
+        new GloboGymTeammate(
+          id.name,
+          id.canThrowBall,
+          id.canDodgeBall,
+          id.hasPaid,
+          id.isHealthy,
+          id.yearsExp,
+          "Globo Gym",
+          "Red",
+          "Purple Cobras"
+        )
+      )
+      globoList.append(globoLI)
+      globoLI.appendChild(document.createTextNode(`${id.name}`))
+      eligibleLI.removeChild(this)
+      eligibleLI.removeChild(avgJoeButton)
+    } else if (
+      id.canThrowBall !== true ||
+      id.canDodgeBall !== true ||
+      id.hasPaid !== true ||
+      id.isHealthy !== true ||
+      id.yearsExperience <= 0
+    ) {
+      alert("Not Eligible")
+    } else {
+      alert("Globo Gym Team Full")
+      eligibleLI.removeChild(globoButton)
+    }
+  })
+  avgJoeButton.addEventListener("click", function () {
+    if (
+      averageJoes.length < 3 &&
+      id.canThrowBall == true &&
+      id.canDodgeBall == true &&
+      id.hasPaid == true &&
+      id.isHealthy == true &&
+      id.yearsExperience > 0
+    ) {
+      averageJoes.push(
+        new AverageJoesTeammate(
+          id.name,
+          id.canThrowBall,
+          id.canDodgeBall,
+          id.hasPaid,
+          id.isHealthy,
+          id.yearsExp,
+          "Average Joes",
+          "Blue",
+          "Joes"
+        )
+      )
+
+      /3 tests//
+// 1.
+// 2.
+// 3.
+// 1. Should create new instance of DodgeBallPlayer
+// 2. Should be able to create instance of AverageJoesTeammate
+// 3. Teammate Objects should have color and mascot keys
+
+if (typeof describe === "function") {
+  describe("DodgeBallPlayer", () => {
+    it("Should create new instance of DodgeBallPlayer", () => {
+      // assert.equal(listOfPlayers.length, 0)
+      const vinceVaughn = new DodgeBallPlayer(
+        "Vince Vaughn",
+        true,
+        true,
+        true,
+        true,
+        16
+      )
+      assert.equal(vinceVaughn.player, "Vince Vaughn")
+    })
+    it("Should prove person becomes a player", () => {
+      assert.equal(globoGym.length, 0)
+      // const vinceVaughn = new DodgeBallPlayer(
+      //   "Vince Vaughn",
+      //   true,
+      //   true,
+      //   true,
+      //   true,
+      //   16
+      // )
+      makePlayer(arrOfPeople[0])
+      // assert.equal()
+      assert.equal(globoGym.length, [1])
+  })
+  describe("GloboGymTeammate", () => {
+    it("Should be able to create instance of AverageJoesTeammate", () => {
+      const vinceVaughnAvgJoe = new AverageJoesTeammate(
+        "Vince Vaughn",
+        true,
+        true,
+        true,
+        true,
+        16,
+        "Blue",
+        "Average Joes"
+      )
+      assert.equal(vinceVaughnAvgJoe.mascot, "Average Joes")
+    })
+    it("Teammate Objects should have color and mascot keys", () => {
+      const vinceVaughnAvgJoe = new AverageJoesTeammate(
+        "Vince Vaughn",
+        true,
+        true,
+        true,
+        true,
+        16,
+        "Average Joes",
+        "Joes"
+        "Blue",
+        "Average Joes"
+      )
+      assert.equal(vinceVaughn.teamName, "Average Joes")
+      assert.equal(vinceVaughnAvgJoe.color, "Blue")
+      assert.equal(vinceVaughnAvgJoe.mascot, "Average Joes")
+    })
+    // it("Should add person to listOfPlayers", () => {
+    //   const player1 = new DodgeBallPlayer("Rick Astley")
+    // })
+  })
+}
+ ProTip! Use n and p 
